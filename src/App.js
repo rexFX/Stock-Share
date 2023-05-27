d5ca2f4f0f7d4280e256d331c9b264fbe58c1c3a
Shivam Karn <shivam3511@gmail.com>
2023-05-27 14:36:51 +0530

    trying mobile fix

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Dashboard from "./pages/Dashboard";
 function App() {
   return (
     <div className="fixed h-[100vh] w-[100vw] bg-[#F8EDEB]">
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      <div className="h-full w-full overflow-y-auto">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
     </div>
   );
 }
