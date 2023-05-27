116380087740947c8c1fc094cee35dbb836ee376
Shivam Karn <shivam3511@gmail.com>
2023-05-27 15:45:11 +0530

    changed responsiveness of page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -173,107 +173,113 @@ const Dashboard = () => {
 				)}
 			</nav>
 
-			{data && (
-				<div className="h-[60%] w-full flex justify-center overflow-x-auto">
-					<ChartRender
-						stockData={data}
-						priceDate={dateAndPriceSetter}
-					/>
-				</div>
-			)}
-			{companyPresent && !data && (
-				<div className="mt-20 w-full h-1/3 flex justify-center items-center">
-					<Spinner />
-				</div>
-			)}
-
-			<div
-				className={`w-full text-center ${
-					companyPresent
-						? "h-auto overflow-y-auto"
-						: "h-[50%] flex justify-center items-center"
-				}`}
-			>
-				{companyPresent ? (
-					<>
-						<h2 className="font-ubuntu text-lg md:text-2xl lg:text-4xl">
-							{date.length > 0
-								? `The stock price of ${company} on ${date} is ${price} ${currency}`
-								: `Select a date from the graph`}
-						</h2>
-						{date.length > 0 && (
-							<div>
-								<button
-									className="m-4 w-30 border-2 p-2 border-black rounded-sm font-ubuntu bg-[#F9DCC4] hover:bg-[#FEC89A] transition-colors"
-									onClick={() => {
-										window.location.href =
-											"mailto:?subject=Me&body=Hello!";
-									}}
-								>
-									Share via email
-								</button>
-								<button className="m-4 w-30 border-2 p-2 border-black rounded-sm font-ubuntu bg-[#F9DCC4] hover:bg-[#FEC89A] transition-colors">
-									Share via Whatsapp
-								</button>
-							</div>
-						)}
-					</>
-				) : (
-					<div className="h-full w-[80%] md:w-[75%] lg:w-1/3 flex-col justify-center items-center mt-36">
-						<h2 className="font-ubuntu text-center text-lg lg:text-4xl">
-							Company Search
-						</h2>
-						<div className="flex justify-around items-center">
-							<input
-								className="border-2 border-black rounded-sm p-2 mt-4 mr-2 w-28 md:w-44 lg:w-72 font-ubuntu"
-								type="text"
-								value={company}
-								placeholder="Company"
-								onChange={(e) => setCompany(e.target.value)}
-							/>
-							<input
-								className="border-2 border-black rounded-sm p-2 mt-4 w-28 md:w-44 lg:w-72 font-ubuntu"
-								type="text"
-								value={exchange}
-								placeholder="Exchange"
-								onChange={(e) => setExchange(e.target.value)}
-							/>
-						</div>
-						<div className="w-full flex justify-center">
-							<button
-								className="m-4 w-40 border-2 p-2 border-black rounded-sm font-ubuntu text-lg bg-[#F9DCC4] hover:bg-[#FEC89A] transition-colors"
-								onClick={companyFinder}
-							>
-								{loading ? (
-									<div className="flex justify-center items-center">
-										<Spinner />
-									</div>
-								) : (
-									"Search"
-								)}
-							</button>
-						</div>
-						{companyList.length > 0 && (
-							<div className="w-full max-h-96 flex-col overflow-y-auto border-2 border-black rounded-lg p-4">
-								{companyList.map((company) => {
-									return (
-										<button
-											key={company.symbol}
-											className="w-full mb-1 border-2 p-2 border-black rounded-sm font-ubuntu text-lg bg-[#F9DCC4] hover:bg-[#FEC89A] transition-colors text-left pl-8"
-											onClick={() => {
-												setCompany(company.name);
-												setCompanyPresent(true);
-												stockDetails(company.symbol);
-											}}
-										>
-											{company.name}
-										</button>
-									);
-								})}
-							</div>
-						)}
+			<div className="flex-col overflow-y-auto">
+				{data && (
+					<div className="h-[60%] w-full flex justify-center overflow-x-auto">
+						<ChartRender
+							stockData={data}
+							priceDate={dateAndPriceSetter}
+						/>
 					</div>
 				)}
+				{companyPresent && !data && (
+					<div className="mt-20 w-full h-1/3 flex justify-center items-center">
+						<Spinner />
+					</div>
+				)}
+
+				<div
+					className={`w-full text-center ${
+						companyPresent
+							? "h-auto"
+							: "h-[50%] flex justify-center items-center"
+					}`}
+				>
+					{companyPresent ? (
+						<>
+							<h2 className="mt-8 font-ubuntu text-lg md:text-2xl lg:text-4xl">
+								{date.length > 0
+									? `The stock price of ${company} on ${date} is ${price} ${currency}`
+									: `Select a date from the graph`}
+							</h2>
+							{date.length > 0 && (
+								<div>
+									<button
+										className="m-4 w-30 border-2 p-2 border-black rounded-sm font-ubuntu bg-[#F9DCC4] hover:bg-[#FEC89A] transition-colors"
+										onClick={() => {
+											window.location.href =
+												"mailto:?subject=Me&body=Hello!";
+										}}
+									>
+										Share via email
+									</button>
+									<button className="m-4 w-30 border-2 p-2 border-black rounded-sm font-ubuntu bg-[#F9DCC4] hover:bg-[#FEC89A] transition-colors">
+										Share via Whatsapp
+									</button>
+								</div>
+							)}
+						</>
+					) : (
+						<div className="h-full w-[80%] md:w-[75%] lg:w-1/3 flex-col justify-center items-center mt-36">
+							<h2 className="font-ubuntu text-center text-lg lg:text-4xl">
+								Company Search
+							</h2>
+							<div className="flex justify-around items-center">
+								<input
+									className="border-2 border-black rounded-sm p-2 mt-4 mr-2 w-28 md:w-44 lg:w-72 font-ubuntu"
+									type="text"
+									value={company}
+									placeholder="Company"
+									onChange={(e) => setCompany(e.target.value)}
+								/>
+								<input
+									className="border-2 border-black rounded-sm p-2 mt-4 w-28 md:w-44 lg:w-72 font-ubuntu"
+									type="text"
+									value={exchange}
+									placeholder="Exchange"
+									onChange={(e) =>
+										setExchange(e.target.value)
+									}
+								/>
+							</div>
+							<div className="w-full flex justify-center">
+								<button
+									className="m-4 w-40 border-2 p-2 border-black rounded-sm font-ubuntu text-lg bg-[#F9DCC4] hover:bg-[#FEC89A] transition-colors"
+									onClick={companyFinder}
+								>
+									{loading ? (
+										<div className="flex justify-center items-center">
+											<Spinner />
+										</div>
+									) : (
+										"Search"
+									)}
+								</button>
+							</div>
+							{companyList.length > 0 && (
+								<div className="w-full max-h-96 flex-col overflow-y-auto border-2 border-black rounded-lg p-4">
+									{companyList.map((company) => {
+										return (
+											<button
+												key={company.symbol}
+												className="w-full mb-1 border-2 p-2 border-black rounded-sm font-ubuntu text-lg bg-[#F9DCC4] hover:bg-[#FEC89A] transition-colors text-left pl-8"
+												onClick={() => {
+													setCompany(company.name);
+													setCompanyPresent(true);
+													stockDetails(
+														company.symbol
+													);
+												}}
+											>
+												{company.name}
+											</button>
+										);
+									})}
+								</div>
+							)}
+						</div>
+					)}
+				</div>
 			</div>
 		</div>
 	);
