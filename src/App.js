6f8707751ce3866bb190a9a16914016c880cd3f6
Shivam Karn <shivam3511@gmail.com>
2023-05-29 14:47:26 +0530

    added backend initialization useeffect hook

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_BACKEND}/`, {
+      method: "GET",
+    }).then((res) => console.log("backend started"));
+  }, []);
+
   return (
     <div className="fixed h-[100vh] w-[100vw] bg-[#F8EDEB]">
       <div className="h-full w-full overflow-y-auto">
